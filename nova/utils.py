08872907ed5ff6db5a0ea3100bf01c004a7fb4d8
René Ribaud <rribaud@redhat.com>
2022-06-16 16:22:48 +0200

    Attach Manila shares via virtiofs (manila abstraction)
    This patch is inspired by /nova/volume/cinder.py, it is an abstraction to
    the manila service.
    
    Manila is the OpenStack Shared Filesystems service.
    These series of patches implement changes required in Nova to allow the shares
    provided by Manila to be associated with and attached to instances using
    virtiofs.
    
    Implements: blueprint libvirt-virtiofs-attach-manila-shares
    Depends-On: https://review.opendev.org/c/openstack/openstacksdk/+/889519
    
    Change-Id: I44ab37ec2c15fcfc351c42216660bda39461b163

--- nova/utils.py
+++ nova/utils.py
@@ -966,7 +966,7 @@ def get_ksa_adapter(service_type, ksa_auth=None, ksa_session=None,
         min_version=min_version, max_version=max_version, raise_exc=False)
 
 
-def get_sdk_adapter(service_type, check_service=False):
+def get_sdk_adapter(service_type, check_service=False, **kwargs):
     """Construct an openstacksdk-brokered Adapter for a given service type.
 
     We expect to find a conf group whose name corresponds to the service_type's
@@ -977,6 +977,9 @@ def get_sdk_adapter(service_type, check_service=False):
                          is to be constructed.
     :param check_service: If True, we will query the endpoint to make sure the
             service is alive, raising ServiceUnavailable if it is not.
+    :param kwargs: Additional arguments to pass to the Adapter constructor.
+                   Mainly used to pass microversion to a specific service,
+                   e.g. shared_file_system_api_version="2.82".
     :return: An openstack.proxy.Proxy object for the specified service_type.
     :raise: ConfGroupForServiceTypeNotFound If no conf group name could be
             found for the specified service_type.
@@ -987,12 +990,16 @@ def get_sdk_adapter(service_type, check_service=False):
     try:
         conn = connection.Connection(
             session=sess, oslo_conf=CONF, service_types={service_type},
-            strict_proxies=check_service)
+            strict_proxies=check_service, **kwargs)
     except sdk_exc.ServiceDiscoveryException as e:
         raise exception.ServiceUnavailable(
             _("The %(service_type)s service is unavailable: %(error)s") %
             {'service_type': service_type, 'error': str(e)})
-    return getattr(conn, service_type)
+    # The replace('-', '_') below is to handle service names that use
+    # hyphens and SDK attributes that use underscores.
+    # e.g. service name --> sdk attribute
+    #      'shared-file-system' --> 'shared_file_system'
+    return getattr(conn, service_type.replace('-', '_'))
 
 
 def get_endpoint(ksa_adapter):
