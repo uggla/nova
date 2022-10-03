47279cfa2cbb51cdacee46a76f5a697889273184
René Ribaud <rribaud@redhat.com>
2022-10-03 17:56:06 +0200

    Support resuming an instance with shares (compute manager part)
    Allow to resume an instance with shares attached.
    
    Manila is the OpenStack Shared Filesystems service.
    These series of patches implement changes required in Nova to allow the shares
    provided by Manila to be associated with and attached to instances using
    virtiofs.
    
    Implements: blueprint libvirt-virtiofs-attach-manila-shares
    Change-Id: I41639e51b624be0d09f8dad25e66cb8bd0185311

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -7067,6 +7067,7 @@ class ComputeManager(manager.Manager):
             context, instance.uuid)
         block_device_info = self._get_instance_block_device_info(
             context, instance, bdms=bdms)
+        share_info = self._get_share_info(context, instance)
 
         compute_utils.notify_about_instance_action(context, instance,
             self.host, action=fields.NotificationAction.RESUME,
@@ -7077,7 +7078,7 @@ class ComputeManager(manager.Manager):
         with self._error_out_instance_on_exception(context, instance,
              instance_state=instance.vm_state):
             self.driver.resume(context, instance, network_info,
-                               block_device_info)
+                               block_device_info, share_info)
 
         instance.power_state = self._get_power_state(instance)
 
