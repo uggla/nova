aac2dcadf426ac7db5afeddc066af39f0ed1614a
René Ribaud <rribaud@redhat.com>
2023-04-25 14:51:46 +0200

    Deletion of associated share mappings on instance deletion
    The intended action of this patch is to eliminate the mappings of shares
    associated with an instance once the instance is deleted first.
    
    Implements: blueprint libvirt-virtiofs-attach-manila-shares
    Change-Id: I920266e5ff1397f6324344e21c56e243e9f02417

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -3147,6 +3147,7 @@ class ComputeManager(manager.Manager):
                     phase=fields.NotificationPhase.START, bdms=bdms)
 
         network_info = instance.get_network_info()
+        share_info = self._get_share_info(context, instance)
 
         # NOTE(arnaudmorin) to avoid nova destroying the instance without
         # unplugging the interface, refresh network_info if it is empty.
@@ -3185,6 +3186,12 @@ class ComputeManager(manager.Manager):
             self._try_deallocate_network(context, instance, requested_networks)
 
         timer.restart()
+
+        if share_info:
+            for share in share_info:
+                self.umount_share(context, instance, share)
+                share.delete()
+
         connector = None
         for bdm in vol_bdms:
             try:
